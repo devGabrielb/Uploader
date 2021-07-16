2addc77d15c96aeb83f9685aea4d2afe0f198a00
devGabrielb <dev.gabrielb1@gmail.com>
2021-07-16 08:40:22 -0300

    feat: add component upload

---
+++ src/pages/Home/components/upload/upload.js
@@ -0,0 +1,79 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-console */
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import UploadStyle from './style/upload.module.css';
+
+const Upload = (props) => {
+    const { onUpload } = props;
+
+    const colorClasses = [
+        UploadStyle.colorDefault,
+        UploadStyle.colorError,
+        UploadStyle.colorSuccess
+    ];
+
+    const dragResult = (isDragActive, isDragReject, acceptedFiles) => {
+        console.log({ isDragActive, isDragReject, acceptedFiles });
+
+        if (isDragActive) {
+            return UploadStyle.dragActive;
+        }
+        if (isDragReject && !isDragActive) {
+            return UploadStyle.dragReject;
+        }
+        return UploadStyle.dragDefault;
+    };
+
+    const renderDragMessage = (isDragActive, isDragReject) => {
+        if (isDragActive) {
+            return (
+                <p
+                    className={`${UploadStyle.uploadMessage} ${colorClasses[2]}`}
+                >
+                    Solte os arquivos aqui
+                </p>
+            );
+        }
+        if (isDragReject && !isDragActive) {
+            return (
+                <p
+                    className={`${UploadStyle.uploadMessage} ${colorClasses[1]}`}
+                >
+                    Arquivo não suportado
+                </p>
+            );
+        }
+
+        return (
+            <p className={`${UploadStyle.uploadMessage} ${colorClasses[0]}`}>
+                Arraste e solte um arquivo CSV aqui
+            </p>
+        );
+    };
+    const {
+        getRootProps,
+        getInputProps,
+        isDragActive,
+        acceptedFiles,
+        isDragReject
+    } = useDropzone({ accept: '.csv', onDropAccepted: onUpload, maxFiles: 1 });
+
+    return (
+        <div>
+            <div
+                {...getRootProps()}
+                className={`${UploadStyle.dropzone} ${dragResult(
+                    isDragActive,
+                    isDragReject,
+                    acceptedFiles
+                )}`}
+            >
+                <input {...getInputProps()} />
+                {renderDragMessage(isDragActive, isDragReject)}
+            </div>
+        </div>
+    );
+};
+
+export default Upload;
